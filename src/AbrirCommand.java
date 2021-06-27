3920d7f4ccc9ff37eef58602ef1ae5b766f2a346
CrisKoch <cristinak.iftm@gmail.com>
2021-06-27 19:36:01 -0300

    Exemplo completo

---
+++ src/AbrirCommand.java
@@ -0,0 +1,22 @@
+
+public class AbrirCommand implements Command {
+	
+	private Portao portao;
+	
+	public AbrirCommand(Portao portao) {
+		this.portao = portao;
+	}
+	
+	@Override
+	public void execute() {
+		portao.abrir();
+
+	}
+
+	@Override
+	public void undo() {
+		portao.fechar();
+
+	}
+
+}
---
+++ src/Portao.java
@@ -0,0 +1,29 @@
+
+public class Portao {
+	public final static int ABERTO = 0;
+	public final static int FECHADO = 1;
+	
+	private int estado = 0;
+	public int getEstado() {
+		return estado;
+	}
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+	public void abrir() {
+		this.estado = ABERTO;
+		System.out.println("O portão abriu.");
+		
+	}
+	public void fechar() {
+		this.estado = FECHADO;
+		System.out.println("O portão fechou.");
+	}
+	public String verEstado() {
+		if(estado == ABERTO) {
+			return "O portao esta aberto.";
+		}else {
+			return "O portao esta fechado";
+		}
+	}
+}
